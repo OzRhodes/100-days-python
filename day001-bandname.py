2e98bbb157f40fc213e216c877be1a8c79599ed1
Oz Rhodes <44510679+OzRhodes@users.noreply.github.com>
2023-02-05 18:16:46 +0000

    Day one task completed
    Take 2 string inputs and combine them as an output

---
+++ day001-bandname.py
@@ -0,0 +1,17 @@
+# Requirements:
+# Take 2 inputs as strings and combine them to create a third stgring for printing out a Band Name
+
+# Welcome to the Band Name Generator.
+# What is the name of the city you grew up in?
+# What was your pet's name'
+
+# Your bands name could be...
+
+part1 = input("What is the name of the city you grew up in? ").capitalize()
+part2 = input("What was your pet's name? ").capitalize()
+
+
+print(f'Your band name could be {part1} {part2}')
+
+
+
