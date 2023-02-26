a5f0eab7ee097bfbb7bb53a33e0687b7c9bb1d88
Oz Rhodes <44510679+OzRhodes@users.noreply.github.com>
2023-02-26 16:12:52 +0000

    Upload pythonsita turtle and turtle2.py that run turtle UI code on pythonista

---
+++ turtle_snake.py
@@ -0,0 +1,19 @@
+
+from turtle import Screen,Turtle
+
+# initial screen setup
+screen = Screen()
+screen.setup(width=600, height =600)
+screen.bgcolor("black")
+screen.title("snake")
+
+# create 3 squares
+
+
+
+screen.exitonclick()
+
+
+
+
+
