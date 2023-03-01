7759a7d6f3a60254099ed4864f86ee49b5e52a09
Oz Rhodes <44510679+OzRhodes@users.noreply.github.com>
2023-03-01 21:37:11 +0000

    Initial snake

--- turtle_snake.py
+++ turtle_snake.py
@@ -1,5 +1,6 @@
 
-from turtle import Screen,Turtle
+from turtle import Turtle 
+import random
 
 # initial screen setup
 screen = Screen()
@@ -8,9 +9,31 @@ screen.bgcolor("black")
 screen.title("snake")
 
 # create 3 squares
+turtle.colormode(255)# range of colors
+my_turtle =Turtle()
 
+my_turtle.pensize(15)
+my_turtle.speed("fastest")
+my_turtle.pencolor(r,g,b)
 
-
+def random_color():
+	r = random.randint(0,255)
+	g = random.randint(0,255)
+	b = random.randint(0,255)
+	return(r,g,b)
+	
+	
+	
+for _ in range(200):
+	my_turtle.color(random_color())
+	my_turtle.fwd(10)
+	my_turtle.rt(90)
+	
+	
+	
+	
+	
+	
 screen.exitonclick()
 
 
